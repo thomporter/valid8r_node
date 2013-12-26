612190847c6a00bd8783ce62abad474cd3c92e9c
Thom Porter <thomporter@gmail.com>
2013-12-26 12:01:07 -1000

    Added IP Address Tests #gklst

--- tests/special_fields.js
+++ tests/special_fields.js
@@ -17,6 +17,21 @@ var v = new Valid8r({
 			rules:[
 				{rule:"url"}
 			]
+		},
+		ip_test: {
+			rules: [
+				{rule:"ip"}
+			]
+		},
+		ipv4_test: {
+			rules: [
+				{rule:"ip",v:4}
+			]
+		},
+		ipv6_test: {
+			rules: [
+				{rule:"ip",v:6}
+			]
 		}
 	}
 });
@@ -56,4 +71,53 @@ describe('Valid8r Special Validators', function(){
 		});
 	});
 	
+	describe('ip', function(){
+		
+		it('should not return an error when a valid IPv4 or IPv6 is given', function(){
+			var err = v.validate('ip_test', '5.5.5.5')
+			if (err) throw new Error('IP Rule Failed - error given w/ valid IPv4');
+			var err = v.validate('ip_test', 'fe80:0000:0000:0000:0204:61ff:fe9d:f156')
+			if (err) throw new Error('IP Rule Failed - error given w/ valid IPv6');
+		});
+		it('should return an error when a invalid IPv4 or IPv6 is given', function(){
+			var err = v.validate('ip_test', '256.5.5.5')
+			if (!err) throw new Error('IP Rule Failed - no error given w/ invalid IPv4');
+			var err = v.validate('ip_test', 'xe80:0000:0000:0000:0204:61ff:fe9d:f156')
+			if (!err) throw new Error('IP Rule Failed - no error given w/ invalid IPv6');
+		});
+	});
+	
+	describe('ipv4', function(){
+		it('should return an error when an invalid IPv4 is given', function(){
+			var err = v.validate('ipv4_test', '256.5.5.5')
+			if (!err) throw new Error('IPv4 Rule Failed');
+		});
+		it('should return an error when an IPv6 is given for IPv4', function(){
+			var err = v.validate('ipv4_test', 'fe80:0000:0000:0000:0204:61ff:fe9d:f156')
+			if (!err) throw new Error('IPv4 Rule Failed');
+		});
+		it('should not return an error when a valid IPv4 is given', function(){
+			var err = v.validate('ipv4_test', '255.5.5.5')
+			if (err) throw new Error('IPv4 Rule Failed - detected error in good IP');
+		});
+
+
+	});
+
+	describe('ipv6', function(){
+		it('should return an error when an IPv4 is given for IPv6', function(){
+			var err = v.validate('ipv6_test', '5.5.5.5')
+			if (!err) throw new Error('IPv6 Rule Failed');
+		});
+		it('should return an error when an invalid IPv6 is given', function(){
+			var err = v.validate('ipv6_test', 'xe80:0000:0000:0000:0204:61ff:fe9d:f156')
+			if (!err) throw new Error('IPv6 Rule Failed');
+		});
+
+		it('should not return an error when a valid IPv6 is given', function(){
+			var err = v.validate('ipv6_test', 'fe80:0000:0000:0000:0204:61ff:fe9d:f156')
+			if (err) throw new Error('IPv6 Rule Failed - detected error in good IPv6');
+		});
+
+	});
 });
